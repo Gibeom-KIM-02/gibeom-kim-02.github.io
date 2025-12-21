37e6b728c260db4d1cef8fca3f492d90303aea27
Gibeom Kim <kkb1198@unist.ac.kr>
2025-12-21 20:02:48 +0900

    Use root base path for GitHub user pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://gibeom-kim-02.github.io',
-  base: '/regular-radiation',
   trailingSlash: 'always',
   integrations: [sitemap()],
 });
